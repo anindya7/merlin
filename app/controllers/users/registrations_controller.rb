85e05d861a44f8cc9a4957775584d0be38fb5d09
tejazt <tejas.tholpadi7@gmail.com>
2017-09-18 18:57:33 +0530

    fixed sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,29 +14,34 @@ class Users::RegistrationsController < Devise::RegistrationsController
     # render json: params
     build_resource(sign_up_params)
     resource.razorpay_payment_id = params[:razorpay_payment_id]
-    response = Razorpay::Payment.fetch(params[:razorpay_payment_id]).capture({amount:100})
-    unless response.nil?
-      resource.save
-      yield resource if block_given?
-      if resource.persisted?
-        if resource.active_for_authentication?
-          set_flash_message! :notice, :signed_up
-          sign_up(resource_name, resource)
-          ReferMailer.welcome_mail(resource).deliver
-          respond_with resource, location: after_sign_up_path_for(resource)
+    if resource.valid?
+      response = Razorpay::Payment.fetch(params[:razorpay_payment_id]).capture({amount:100})
+      unless response.nil?
+        resource.save
+        yield resource if block_given?
+        if resource.persisted?
+          if resource.active_for_authentication?
+            set_flash_message! :notice, :signed_up
+            sign_up(resource_name, resource)
+            ReferMailer.welcome_mail(resource).deliver
+            respond_with resource, location: after_sign_up_path_for(resource)
+          else
+            set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+            expire_data_after_sign_in!
+            respond_with resource, location: after_inactive_sign_up_path_for(resource)
+          end
         else
-          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-          expire_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+          clean_up_passwords resource
+          set_minimum_password_length
+          respond_with resource
         end
       else
-        clean_up_passwords resource
-        set_minimum_password_length
-        respond_with resource
+        flash[:alert] = "Error while signing up"
+        redirect_to new_user_registration_path
       end
     else
-      flash[:notice] = "Error while signing up"
-      redirect_to root_path
+      flash[:alert] = "Error while signing up"
+      redirect_to new_user_registration_path
     end
   end
 
